65004ba5ecc9679bb3db504d0273d3f8be4458ae
masecla22 <supermasecla22@gmail.com>
2024-04-21 17:21:48 +0200

    Update ListMerger.tsx

--- src/components/list/ListMerger.tsx
+++ src/components/list/ListMerger.tsx
@@ -3,22 +3,25 @@ import { useState } from "react";
 import { useSelectedListProvider } from "../../contexts/SelectedListProvider";
 import { UserItemList, UserItemListItemDefinition } from "../adder/ItemList";
 
-function executeMerge(targetList: UserItemList, sourceLists: UserItemList[], deleteSourceLists: boolean,
+function executeMerge(targetListName: string, sourceListNames: string[], deleteSourceLists: boolean,
     setAvailableLists: (lists: { [key: string]: UserItemList }) => void,
-    availableLists: { [key: string]: UserItemList }) {
+    getAvailableLists: () => { [key: string]: UserItemList }) {
 
-    const newItems: Record<string, UserItemListItemDefinition> = {};
+    var newItems: Record<string, UserItemListItemDefinition> = {};
 
-    for (const item of targetList.items) {
-        newItems[item.item] = item;
-    }
+    const targetList = getAvailableLists()[targetListName];
+    const sourceLists = sourceListNames.map((listName) => getAvailableLists()[listName]);
+
+    targetList.items.forEach((item) => {
+        newItems[item.item] = { ...item };
+    });
 
     for (const sourceList of sourceLists) {
         for (const item of sourceList.items) {
             if (newItems[item.item]) {
                 newItems[item.item].amount += item.amount;
             } else {
-                newItems[item.item] = item;
+                newItems[item.item] = { ...item };
             }
         }
     }
@@ -28,7 +31,7 @@ function executeMerge(targetList: UserItemList, sourceLists: UserItemList[], del
         items: Object.values(newItems)
     } as UserItemList;
 
-    const newLists = { ...availableLists, [newList.name]: newList };
+    const newLists = { ...getAvailableLists(), [newList.name]: newList };
     if (deleteSourceLists) {
         for (const sourceList of sourceLists) {
             delete newLists[sourceList.name];
@@ -44,12 +47,11 @@ function ListMerger(props: {
 }) {
     const { open, onClose } = props
 
-    const { availableLists, setAvailableLists } = useSelectedListProvider();
+    const { getAvailableLists, setAvailableLists } = useSelectedListProvider();
 
-    const [targetList, setTargetList] = useState<UserItemList>(availableLists[Object.keys(availableLists)[0]]);
-    const [sourceLists, setSourceLists] = useState<UserItemList[]>([]);
+    const [targetList, setTargetList] = useState<string>(Object.values(getAvailableLists())[0].name);
+    const [sourceLists, setSourceLists] = useState<string[]>([]);
     const [deleteSourceLists, setDeleteSourceLists] = useState<boolean>(false);
-
 
     return <Dialog
         open={open}
@@ -88,14 +90,14 @@ function ListMerger(props: {
                         <TextField
                             select
                             label="Target List"
-                            value={targetList.name}
+                            value={targetList}
                             onChange={(e) => {
-                                setTargetList(availableLists[e.target.value]);
-                                setSourceLists(sourceLists.filter((list) => list !== availableLists[e.target.value]));
+                                setTargetList(e.target.value);
+                                setSourceLists(sourceLists.filter((list) => list !== e.target.value));
                             }}
                             fullWidth
                         >
-                            {Object.keys(availableLists).map((listName) => {
+                            {Object.keys(getAvailableLists()).map((listName) => {
                                 return <MenuItem key={listName} value={listName}>{listName}</MenuItem>
                             })}
                         </TextField>
@@ -103,17 +105,17 @@ function ListMerger(props: {
                 </Grid>
                 <Grid item xs={12} md={8}>
                     <Grid container pl={3}>
-                        {Object.keys(availableLists).map((listName) => {
-                            if (listName === targetList.name) {
+                        {Object.keys(getAvailableLists()).map((listName) => {
+                            if (listName === targetList) {
                                 return null;
                             }
                             return <Grid item xs={12} sm={6} md={6} key={listName}>
-                                <Checkbox checked={sourceLists.includes(availableLists[listName])}
+                                <Checkbox checked={sourceLists.includes(listName)}
                                     onChange={(e) => {
                                         if (e.target.checked) {
-                                            setSourceLists([...sourceLists, availableLists[listName]]);
+                                            setSourceLists([...sourceLists, listName]);
                                         } else {
-                                            setSourceLists(sourceLists.filter((list) => list !== availableLists[listName]));
+                                            setSourceLists(sourceLists.filter((list) => list !== listName));
                                         }
                                     }}
                                 />
@@ -125,8 +127,9 @@ function ListMerger(props: {
             </Grid>
 
             <Box mt={2}>
-                This will {deleteSourceLists ? "move" : "copy"} {sourceLists.map((list) => list.items.map(c => c.amount).reduce((a, b) => a + b, 0)).reduce((a, b) => a + b, 0)} items
-                from {sourceLists.length} lists into {targetList.name} {deleteSourceLists ? "and delete the original lists" : ""}
+                This will {deleteSourceLists ? "move" : "copy"}
+                {sourceLists.map((list) => getAvailableLists()[list].items.map(c => c.amount).reduce((a, b) => a + b, 0)).reduce((a, b) => a + b, 0)} items
+                from {sourceLists.length} lists into {targetList} {deleteSourceLists ? "and delete the original lists" : ""}
                 <br />
                 <br />
                 Prices will be kept from target list if possible, otherwise from whichever list the comes first.
@@ -135,7 +138,7 @@ function ListMerger(props: {
         <DialogActions>
             <Button onClick={onClose}>Cancel</Button>
             <Button onClick={() => {
-                executeMerge(targetList, sourceLists, deleteSourceLists, setAvailableLists, availableLists);
+                executeMerge(targetList, sourceLists, deleteSourceLists, setAvailableLists, getAvailableLists);
                 setSourceLists([]);
                 onClose();
             }}>Confirm</Button>
